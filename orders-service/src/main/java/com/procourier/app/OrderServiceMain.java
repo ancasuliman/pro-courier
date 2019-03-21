14e57539c95c44a81a26851cc12281cf7a8b24c6
ancasuliman <anca.suliman15@yahoo.com>
2019-03-21 20:21:42 +0200

    Added order to repo

---
+++ orders-service/src/main/java/com/procourier/app/OrderServiceMain.java
@@ -0,0 +1,33 @@
+package com.procourier.app;
+
+import com.procourier.model.*;
+import com.procourier.repository.CollectionRepository;
+import spark.Spark;
+
+import java.util.Map;
+
+public class OrderServiceMain {
+    public static void main(String[] args) {
+        final var repository = new CollectionRepository();
+        final var handler = new OrdersHandler(repository);
+
+        final Address address = new Address("DJ", "Craiova", "Nicolae Titulescu");
+        final Seller seller = new Seller("Evomag", address);
+
+        final Address cosminAddress = new Address("DJ", "Craiova", "Ileana Cosanzeana")
+        final Buyer buyer = new Buyer("Cosmin", address);
+
+        final Address courierAddress = new Address("OT", "Slatina", "Cantemir");
+        final Courier courier = new Courier(1L, "ExpressFun", courierAddress,
+                5L, 7L);
+
+        final var products = Map.of(new Product(20L, "Jeans", "Men Jeans", 22L), 4);
+
+        final Order order = new Order(6L, seller, buyer, courier, products);
+        repository.put(order);
+
+        Spark.port(8082);
+        Spark.threadPool(26, 10, 2000);
+        Spark.get("/orders/:id", handler);
+    }
+}
---
+++ orders-service/src/main/java/com/procourier/app/OrdersHandler.java
@@ -0,0 +1,30 @@
+package com.procourier.app;
+
+import com.procourier.model.Order;
+import com.procourier.model.repository.OrderRepository;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class OrdersHandler implements Route {
+    private final OrderRepository repository;
+
+    public OrdersHandler(OrderRepository repository) {
+        this.repository = Objects.requireNonNull(repository);
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        final String id = request.params(":id");
+        final Long longId = Long.valueOf(id);
+
+        final Optional<Order> order = repository.findById(longId);
+        final String info = order.map(o -> o.getId() + " " + o.getBuyer().getName())
+                .orElse("Not found");
+
+        return info;
+    }
+}
